17e55258ccce9e125b8cf8bcfe47defa853009c0
billreus <rine328909144@gmail.com>
2018-10-31 23:00:05 +0800

    ee

---
+++ JAVA/test14/out/artifacts/test14_war_exploded/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: billdog
+  Date: 2018/10/31
+  Time: 14:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page import="java.util.*" language="java" pageEncoding="UTF-8" %>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>登录页面</title>
+</head>
+<body>
+
+<form action="login" method="post">
+  账号: <input type="text" name="name"> <br>
+  密码: <input type="password" name="password"> <br>
+  <input type="submit" value="登录">
+</form>
+
+</body>
+</html>
---
+++ JAVA/test14/src/LoginServlet.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        System.out.println("name:" + name);
+
+        String html = null;
+
+        if ("admin".equals(name) && "123".equals(password))
+            html = "<div style='color:green'>登录成功</div>";
+        else
+            html = "<div style='color:red'>登陆失败</div>";
+        response.setContentType("text/html;charset=UTF-8"); //输出时中文编码
+        PrintWriter pw = response.getWriter();
+        pw.println(html);
+    }
+}
