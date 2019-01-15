9dace745ccedcf498a1ac3e8313cdf5a28097e66
billreus <rine328909144@gmail.com>
2019-01-15 23:01:57 +0800

    1.15

--- Java/杂项/core_Java/base50/src/programme9/programme9.java
+++ Java/杂项/core_Java/base50/src/programme9/programme9.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class programme9 {
     public static void main(String args[]){
-        for(int i=1; i<=1000; i++){
+/*        for(int i=1; i<=1000; i++){
             int sum = 0;
             for(int j=1; j<i/2+1; j++){
                 if(i % j == 0){
@@ -18,6 +18,23 @@ public class programme9 {
                 }
             }
         }
-
+*/
+        int res = 0;
+        for(int nums=576; nums<=580; nums++){
+            int count = 0;
+            int num = nums;
+            while(num>0){
+                int bri = num%2;
+                if(bri == 1) count++;
+                num=num/2;
+            }
+            for(int i=2;i<count;i++){
+                if(count%i==0){
+                    break;
+                }
+                if(i==count-1)res++;
+            }
+        }
+        System.out.print(res);
     }
 }
